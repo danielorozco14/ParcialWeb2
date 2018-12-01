74de7c0e699fd4d49c0e9ff9f40e4613fa84a843
danielorozco14 <00200617@uca.edu.sv>
2018-12-01 16:03:26 -0600

    Finalizado main.js

--- adminMoviles/public/javascripts/main.js
+++ adminMoviles/public/javascripts/main.js
@@ -3,24 +3,93 @@ let app={
         this.addEvents();
     },
     addEvents:function(){
-        var contadorFilas=1;
+        
         fetch("/movil")
         .then(res=>res.json())
         .then(data=>{
+            
             let moviles=document.getElementsByClassName("tablaMoviles")[0];
             moviles.innerHTML=data.reduce((cadena,element)=>{
+                var contadorFilas=1;//VERFIFICAR VARIABLE EN SCOPE ROW MAS ADELANTE
                 return cadena + 
-                `
-                <tr>
+                `<tr>
                     <th scope="row">${contadorFilas}</th>
                     <td class="name">${element.nombre}</td>
                     <td class="publicador">${element.publicador}</td>
                     <td class="fecha">${element.fechaPublicacion}</td>
+                    <td class="opciones">
+                        <a data-id="${element._id}" class= "edit" href="">Editar</a>
+                        <a data-id="${element._id}" class= "delete" href="">Eliminar</a>
+                     </td>
                 </tr>                
-                `
-                
-            })
+                `              
+            contadorFilas++;
+            },"");
+            document.querySelectorAll(".delete").forEach(element=>{//PARA ELIMINAR UN REGISTRO
+                element.addEventListener('click',function(event){
+                    event.preventDefault();
+                    let id =element.getAttribute("data-id");
+                    fetch('/movil/'+id,{
+                        method:'DELETE'
+                    })
+                    .then(res=>res.json())
+                    .then(data=>{
+                        if(data.success){
+                            moviles.removeChild(element.parentElement.parentElement);
+                        }
+                    }).catch(err=>{
+                        console.log('La cague en algo:v '+ err);
+                    });
+                });
+            });
+            document.querySelectorAll(".edit").forEach(element=>{//PARA EDITAR UN REGISTRO
+                element.addEventListener('click',function(evnt){//CHEQUEAR VARIABLE evnt
+                    event.preventDefault();
+                    let id =element.getAttribute("data-id");
+                    fetch('/movil/'+id)
+                    .then(res=>res.json())
+                    .then(data=>{
+                        var formulario=document.forms.guardarMovil;
+                        formulario.nombre.value=data.nombre;
+                        formulario.publicador.value=data.publicador;
+                        formulario.fechaPublicacion.value=data.fechaPublicacion;
+                        formulario.action="/movil/" + data._id;
+                    });                    
+                });
+            });
 
-        })
-    }
+        });
+    }    
 }
+let formulario= document.forms.guardarMovil;
+
+formulario.addEventListener('submit',function(event){
+    event.preventDefault();
+    if(formulario.action=='/movil'){
+        fetch(formulario.action,{
+            method:'POST',
+            body: new URLSearchParams(new FormData(formulario))
+        }).then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            loadContent();
+        });
+    }else{
+        fetch(formulario.action,{
+            method:'PUT',
+            body: new URLSearchParams(new FormData(formulario))
+        }).then (res=>res.json())
+        .then(data=>{
+            if(data.success){
+                formulario.action='/movil';
+                formulario.method='POST';
+                alert('DATOS ACTUALIZADOS')//PARA PROBAR,ELIMINAR DESPUES
+                formulario.publicador.value=formulario.nombre.value=formulario.fechaPublicacion.value="";
+                loadContent();
+            }
+        });
+        loadContent();
+    }
+    
+});
+window.onload=()=>app.init();
